d9c7289f35de1e44fa9c2f644778570afa5e9ef1
francesc <francesc.guillen@gmail.com>
2023-11-23 04:46:21 +0100

    update libraries

--- project.clj
+++ project.clj
@@ -1,21 +1,22 @@
-(defproject cljs-tryton "0.1.2"
+(defproject cljs-tryton "0.1.3"
   :description "clj/cljs client library for tryton"
   :url "https://github.com/fgui/cljs-tryton"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.773"] 
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojurescript "1.11.60"] 
                  [cljs-ajax "0.8.0"]
                  [org.clojure/data.codec "0.1.1"]
-                 [org.clojure/core.async "1.3.610"]
+                 [org.clojure/core.async "1.6.681"]
                  [datawalk "0.1.12"]
-                 [mount "0.1.16"]
-                 [clojure.java-time "0.3.2"]
-                 [yogthos/config "1.1.7"]
+                 [mount "0.1.17"]
+                 [clojure.java-time "1.3.0"]
+                 [yogthos/config "1.2.0"]
                  ]
   :plugins [[lein-doo "0.1.7"]
+            [cider/cider-nrepl "0.38.1"]
             [lein-cljsbuild "1.1.8"]
-            [lein-ancient "0.6.10"]]
+            [lein-ancient "1.0.0-RC3"]]
   
  :cljsbuild
   {:builds
@@ -25,3 +26,4 @@
                       ;;; :source-map "target/main.js.map"
                       :optimizations :whitespace
                       :pretty-print true}}}})
+
